00833002e5c07fe147206ad59de721eecbda87ac
MillionPears <trieuleb4@gmail.com>
2024-06-17 19:05:55 +0700

    update 2

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+
 def hello_world():
+    """
+    Hàm để in ra chuỗi 'Hello, world!'
+    """
     print("Hello, world!")
+
 
 # Gọi hàm hello_world để in ra chuỗi "Hello, world!"
 hello_world()
