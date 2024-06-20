a7748e02a969d5400fd7a9f5bf3ddd3b5109d3b4
MillionPears <trieuleb4@gmail.com>
2024-06-20 18:38:28 +0700

    trieu1

--- helloworld.php
+++ helloworld.php
@@ -1,25 +1,16 @@
 <?php
-/**
- * Plugin Name: Hello World Plugin
- * Description: A simple plugin to print 'Hello World'.
- * Version: 1.0
- * Author: Your Name
- * License: GPL2
- */
 
-// Prevent direct access to the file.
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly.
+declare(strict_types=1);
+
+namespace HelloWorld;
+
+class HelloWorld
+{
+    public function sayHello(): void
+    {
+        echo 'Hello, World!';
+    }
 }
 
-/**
- * Function to print 'Hello World'.
- */
-function print_hello_world() {
-    echo 'Hello World';
-}
-
-
-// Hook the function to 'wp_footer' action.
-add_action( 'wp_footer', 'print_hello_world' );
-?>
+$helloWorld = new HelloWorld();
+$helloWorld->sayHello();
