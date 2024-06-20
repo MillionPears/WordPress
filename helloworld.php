01b6d9499d1820fbead7ee54213ab7faefd8f02c
MillionPears <trieuleb4@gmail.com>
2024-06-20 18:48:09 +0700

    trieu6

--- helloworld.php
+++ helloworld.php
@@ -1,21 +1,3 @@
 <?php
 
-declare(strict_types=1);
-
-namespace HelloWorld;
-
-class HelloWorld
-{
-    public function sayHello(): void
-    {
-        echo 'Hello, World!';
-    }
-}
-
-// Đoạn mã dưới đây chỉ được thực thi khi tệp này không được include hoặc require bởi tệp khác
-if (basename(__FILE__) === basename($_SERVER['SCRIPT_FILENAME'])) {
-    (function () {
-       
-        $helloWorld->sayHello();
-    })();
-}
+echo 'Hello, World!';
